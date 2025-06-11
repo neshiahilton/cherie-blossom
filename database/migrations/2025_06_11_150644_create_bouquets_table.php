db8e75eb5bcf744bcf6b9ca73b90fa3e458eaa10
Neshia Hilton <taneshia.422024002@civitas.ukrida.ac.id>
2025-06-11 22:23:26 +0700

    Initial commit - Laravel project

---
+++ database/migrations/2025_06_11_150644_create_bouquets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bouquets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama bouquet
+            $table->text('description')->nullable(); // Deskripsi bunga
+            $table->decimal('price', 8, 2); // Harga
+            $table->string('image')->nullable(); // Path gambar
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bouquets');
+    }
+};
